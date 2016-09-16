ad14ce2c5aebec73dba6490c12723d991431c597
JunseoYoun <junsueg5737@gmail.com>
2016-09-16 09:57:15 +0900

    * Added Object Fracture

---
+++ Assets/Script/Ultimate Game Tools/Fracturing/Sample Scene Data/Scripts/LogicGlobalFracturing.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LogicGlobalFracturing : MonoBehaviour
+{
+    [HideInInspector]
+    public static bool HelpVisible = true;
+
+    void Start()
+    {
+        HelpVisible = true;
+
+        Invoke("Reset", 7.0f);
+    }
+
+    void Reset()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public static void GlobalGUI()
+    {
+        GUI.Box(new Rect(0, 0, 400, 420), "");
+        GUI.Box(new Rect(0, 0, 400, 420), "-----Ultimate Fracturing & Destruction Tool-----");
+        GUILayout.Space(40);
+        GUILayout.Label("Press F1 to show/hide this help window");
+        GUILayout.Label("Press 1-" + UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings + " to select different sample scenes");
+        GUILayout.Space(20);
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i)) UnityEngine.SceneManagement.SceneManager.LoadScene(i);
+        }
+
+        if(Input.GetKeyDown(KeyCode.F1))
+        {
+            HelpVisible = !HelpVisible;
+        }
+    }
+}
